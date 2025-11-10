32909787958ae0e7571ca08713d25c0bf5cbbb8e
lulu <johndoe@example.com>
2025-11-10 20:45:35 +0100

    erafsqe

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def accueil():
+    return render_template('index.html')
+
+@app.route('/catalogue')
+def catalogue():
+    return render_template('catalogue.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
