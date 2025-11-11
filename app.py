8607353c871e6a331cd8456df4802fb769769a90
lulu <johndoe@example.com>
2025-11-11 11:23:19 +0100

    dz

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def accueil():
 def catalogue():
     return render_template('catalogue.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
